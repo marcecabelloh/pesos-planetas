b0b5466b7bc451eedc9e24f6400eda6fae03ec54
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-08 19:34:06 -0300

    html y js función calcular peso

---
+++ assets/js/archivo.js
@@ -0,0 +1,43 @@
+var usuario= prompt("¿Cuál es tu peso?");
+var planeta= prompt("elige un planeta : 1 es mercurio, 2 es Venus, 3 es marte, 4 es jupiter, 5 es saturno, 6 es Urano, 7 es Neptuno, 8 es Plutón")
+var peso = parseInt(usuario);
+var g_tierra= 9.8;// quedaría cambiar el prompt por input y agregar las gravedades de mercurio, venus, saturno, urano, neptuno y pluton
+var g_marte= 3.7;
+var g_jupiter= 24.8;
+var pesoFinal;
+var nombre;
+if(planeta == 1){
+  pesoFinal= peso * g_mercurio/g_tierra;
+  nombre = "Mercurio";
+}
+if(planeta == 2) {
+  pesoFinal= peso * g_venus/g_tierra;
+  nombre = "Venus";
+}
+if(planeta == 3){
+  pesoFinal= peso * g_marte/g_tierra;
+  nombre= "Marte";
+}
+if(planeta == 4){
+  pesoFinal= peso * g_jupiter/g_tierra;
+  nombre= "Júpiter";
+}
+if (planeta == 5) {
+  pesoFinal= peso * g_saturno/g_tierra;
+  nombre= "Saturno";
+}
+if(planeta == 6){
+  pesoFinal= peso * g_urano/g_tierra;
+  nombre="Urano"
+}
+if (planeta == 7) {
+  pesoFinal= peso * g_neptuno/g_tierra;
+  nombre="Neptuno";
+}
+if (planeta == 8) {
+  pesoFinal= peso * g_pluton/g_tierra;
+  nombre="Plutón";
+}
+
+pesoFinal= parseInt(pesoFinal)
+alert("tu peso en " + nombre + " es : " + pesoFinal + " kilos")
